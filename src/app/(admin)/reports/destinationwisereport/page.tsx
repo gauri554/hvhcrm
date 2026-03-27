00ec97925caf0cd9bdf2888c9a1e420993e0c672
Gauri Raut <gauriraut095@gmail.com>
2026-03-27 18:45:52 +0530

    innitially added

---
+++ src/app/(admin)/reports/destinationwisereport/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageTitle from "@/components/PageTitle";
+import DestinationWiseReport from "./DestinationWiseReport"
+
+const DestinationWiseReportPage =()=>{
+    return (
+        <>
+        <PageTitle title="Source Wise Report"/>
+        <DestinationWiseReport/>
+        </>
+    )
+
+};
+
+export default DestinationWiseReportPage;
